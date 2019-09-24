12d92e8888bef570df62245a099a13ab01d12ff9
于源广 <1035883599@qq.com>
2019-09-25 01:02:03 +0800

    v-model功能

--- core/instance/directives/vmodel.js
+++ core/instance/directives/vmodel.js
@@ -3,7 +3,6 @@ import { setValue } from "../../util/util.js"
 
 export function vmodel(vm,elm,data){
     elm.oninput = function(e){
-        console.log(elm.value)
         setValue(vm,data,elm.value)  //vue对象，该元素绑定的属性，该元素的value
     }
 }
